2232fb92673606a11c9739e2468a591bb653a21e
dinonuggetzz <ranisha1410@gmail.com>
2025-05-18 18:11:42 +0200

    flip but backwards

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const cards = document.querySelectorAll('.card');
+
+function flipCard(){
+    // console.log('I was clicked!');
+    // console.log(this);
+    this.classList.toggle('flip');
+
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
 
