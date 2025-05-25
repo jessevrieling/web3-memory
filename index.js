153dac61c8f95d660a6dae3a7625b68bf82411b8
dinonuggetzz <ranisha1410@gmail.com>
2025-05-25 20:58:57 +0200

    fixed flip

--- index.js
+++ index.js
@@ -2,19 +2,77 @@
 function renderCards(){
 let gridSize = document.getElementById("size").value;
 let character = document.getElementById("character").value
+document.getElementById("character").addEventListener("change", renderCards);
+document.getElementById("size").addEventListener("change", renderCards);
 let grid = '';
+    const images = [
+        'pair1.jpg', 'pair2.jpg',
+        'pair3.jpg', 'pair4.jpg',
+        'pair5.jpg', 'pair6.jpg',
+        'pair7.jpg', 'pair8.jpg',
+        'pair9.jpg', 'pair10.jpg',
+        'pair11.jpg', 'pair12.jpg',
+        'pair13.jpg', 'pair14.jpg',
+        'pair15.jpg', 'pair16.jpg'
+    ];
+
+    // Shuffle images                              
+    let pairsNeeded = gridSize / 2;
+    let selectedImages = images.sort(() => 0.5 - Math.random())
+    .sort(() => 0.5 - Math.random())
+    .slice(0, pairsNeeded);
+    let pairedImages = [...selectedImages, ...selectedImages];
+    pairedImages.sort(() => 0.5 - Math.random());
+
+
     for (let i = 0; i < gridSize; i++) {
         let backFaceContent = (character === "count") ? i : character;
         grid += `
             <div class="card" id="card${i}">
                 <div class="back-face">${backFaceContent}</div>
-                <div class="front-face">${i}</div>
+                <div class="front-face"><img src="Style/Pictures/${pairedImages[i]}" id="pair${i}"/></div>
             </div>`;
     }
 
     document.getElementById("memory-grid").innerHTML = grid;
+    attachFlipListeners();
 }
 renderCards();
+
+function attachFlipListeners() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('click', flipCard);
+    });
+}
+
+// Method to flip cards
+const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+function flipCard(){
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        //on first click
+        hasFlippedCard = true;
+        firstCard = this;
+        console.log(firstCard.id)
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+        console.log(secondCard.id)
+
+        if(firstCard.id === secondCard.id){
+            firstCard.removeEventlistener('click', flipCard);
+            secondCard.removeEventlistener('click', flipCard)
+            console.log('pair')
+        }
+        console.log('executed')
+    }
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
+
 
 // Timer for the game
 let totalTime = 100;
