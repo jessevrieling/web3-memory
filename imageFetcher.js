cb98d6bcf476174e745506dd64782bf429b3b680
dinonuggetzz <ranisha1410@gmail.com>
2025-06-04 12:48:56 +0200

    image from meme api

--- imageFetcher.js
+++ imageFetcher.js
@@ -1,14 +1,30 @@
 export default async function fetchImages(imageCount) {
 	let images = []
-	const url = "https://picsum.photos/200"
+	// const url = "https://picsum.photos/200"
+	// const url= fetchFromJson();
 	
 	for(let i = 0; i < imageCount; i++) {
-		const response = await fetch(url)
-		const blob = await response.blob()
-		const objectUrl = URL.createObjectURL(blob)
+		
+		// const response = await fetch(url)
+		// const blob = await response.blob()
+		// const objectUrl = URL.createObjectURL(blob)
 
-		images.push({ url: objectUrl, pairId: `pair-${i}` });
-	}
-
+		// images.push({ url: objectUrl, pairId: `pair-${i}` });
+		// if(){
+		const response = await fetchMemesFromJson(i)
+		images.push({ url: response, pairId: `pair-${i}` });
+		// }
+		
+ 	}
 	return images;
+
+ 	async function fetchMemesFromJson(index){
+		return fetch('https://api.imgflip.com/get_memes')
+		.then(res => res.json())
+		.then(data => {
+			return data.data.memes[index].url
+			})
+
+		.catch(error => console.error());
+	};
 }
