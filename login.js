a9c9a201b90a6d0341862cb590ccfa882ee9a7b9
Jesse <jessevrieling@gmail.com>
2025-06-07 15:13:54 +0200

    Added email to credentials

--- login.js
+++ login.js
@@ -6,17 +6,19 @@ document.getElementById("login-form").addEventListener("submit", async function(
     }
 
     const username = document.getElementById("username-input").value
+    const email = document.getElementById("email-input").value;
     const password = document.getElementById("password-input").value
 
-    await sendCredentials(username, password)
+    await sendCredentials(username, email, password)
 })
 
-async function sendCredentials(username, password) {
+async function sendCredentials(username, email, password) {
     fetch("http://localhost:8000/memory/register", {
         method: "post",
 
         body: JSON.stringify({
             username: username,
+            email: email,
             password: password
         })
     }).then(response => console.log(response))
