03399235c22dd2b94fa3eb1244e5bfc5b3a5e517
Jesse <jessevrieling@gmail.com>
2025-06-08 09:29:16 +0200

    Added id's to input fields

--- login.js
+++ login.js
@@ -1,1 +1,12 @@
 const overlay = document.getElementById("loading-overlay");
+
+document.getElementById("register-form").addEventListener("submit", async function(e) {
+    e.preventDefault();
+
+    overlay.style.display = "flex"; // block input
+
+    const username = document.getElementById("username-input").value
+    const password = document.getElementById("password-input").value
+
+    await sendCredentials(username, email, password)
+})
