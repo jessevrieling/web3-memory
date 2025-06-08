759cf4114667a9352ff362b6a3f74aa95b89e583
Jesse <jessevrieling@gmail.com>
2025-06-08 09:38:31 +0200

    Added credentials check

--- login.js
+++ login.js
@@ -8,5 +8,23 @@ document.getElementById("register-form").addEventListener("submit", async functi
     const username = document.getElementById("username-input").value
     const password = document.getElementById("password-input").value
 
-    await sendCredentials(username, email, password)
+    await sendCredentials(username, password)
 })
+
+async function sendCredentials(username, password) {
+    let messageText = document.getElementById("register-message");
+
+    fetch("http://localhost:8000/memory/register", {
+        method: "post",
+
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    }).then(response => {
+        console.log(response)
+        overlay.style.display = "none";
+
+        window.location.replace("/");
+    }).catch(() => {messageText.innerHTML = "Onjuiste gebruikersnaam of wachtwoord"})
+}
